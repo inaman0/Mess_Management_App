2765ea3c637d2bfcaba6d66ece97af9b8e69fc20
Kartikeya Dimri <Kartikeya.Dimri@iiitb.ac.in>
2025-06-17 19:55:24 +0530

    changed admin layout

--- frontend/src/admin/layouts/AdminLayout.tsx
+++ frontend/src/admin/layouts/AdminLayout.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from '../components/Navbar';
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Navbar from '../components/Navbar';
 
-const AdminLayout: React.FC = () => {
+// const AdminLayout: React.FC = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//       <ToastContainer
+//         position="top-right"
+//         autoClose={3000}
+//         hideProgressBar={false}
+//         closeOnClick
+//         pauseOnHover
+//         theme="light"
+//       />
+//     </>
+//   );
+// };
+
+// export default AdminLayout;
+
+import { Outlet } from 'react-router-dom'
+import AdminSidebar from '../components/AdminSideBar'
+import Background from '../components/Background'
+
+const AdminLayout = () => {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-        theme="light"
-      />
-    </>
-  );
-};
+    <div className='admin-layout'>  
+        <Background />
+        <AdminSidebar />
+        <div className="admin-content">
+          <Outlet />
+        </div>
+    </div>
+  )
+}
 
-export default AdminLayout;
+export default AdminLayout
