677d8ba589637ea7f1366b0529001d31832c8417
Trisha <trisha.edward2005@gmail.com>
2025-06-24 23:11:54 +0530

    styling of sickmeal and feedback pages of user pages

--- frontend/src/user/pages/SickMeal.tsx
+++ frontend/src/user/pages/SickMeal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CreateSick_meal from '../../components/Resource/CreateSick_meal'
 import './SickMeal.css'
 
+
 const Sickmeal = () => {
   return (
     <div className='d-flex justify-content-center align-items-center vh-90'>
