a14b4bcba9746101b14ecfc66805f4e51bd7e567
Kartikeya Dimri <Kartikeya.Dimri@iiitb.ac.in>
2025-07-02 16:16:50 +0530

    page404 v2

--- frontend/src/admin/pages/Page404.tsx
+++ frontend/src/admin/pages/Page404.tsx
@@ -4,14 +4,16 @@ import './Page404.css';
 
 const Page404 = () => {
   return (
-    <div className="page404-container">
-      <div className="page404-content">
-        <h1 className="page404-code">404</h1>
-        <p className="page404-message">The page you're looking for doesn't exist.</p>
-        <Link to="/admin" className="page404-button">Go Back to Dashboard</Link>
+    <>
+      <h2 className='title'>Invalid URL</h2>
+      <div className="page404-container">
+        <div className="page404-content">
+          <h1 className="page404-code">404</h1>
+          <p className="page404-message">The page you are looking for does not exist.</p>
+          <Link to="/admin" className="page404-button">Return to Dashboard</Link>
+        </div>
       </div>
-      
-    </div>
+    </>
   );
 };
 
