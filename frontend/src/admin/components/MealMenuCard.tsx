c620b528083c673fffce56d6585b3a587f66e7a1
Kartikeya Dimri <Kartikeya.Dimri@iiitb.ac.in>
2025-07-10 12:15:36 +0530

    added weekly ratings page

--- frontend/src/admin/components/MealMenuCard.tsx
+++ frontend/src/admin/components/MealMenuCard.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./MealMenuCard.css";
-import ReadReview from "../../components/Resource/ReadReview";
 
 interface MenuItemCardProps {
   Dish_name: string;
   type: string;
   id: string;
   isFeast: boolean;
+  mealType?: string;
 }
 
-const MealMenuCard: React.FC<MenuItemCardProps & { mealType?: string }> = ({
+const MealMenuCard: React.FC<MenuItemCardProps> = ({
   Dish_name,
   type,
   isFeast,
   mealType,
 }) => {
-  // Set text color class based on type
   const typeColorClass =
     type.toLowerCase() === "veg" ? "menu-item-veg" : "menu-item-nonveg";
+
 
   return (
     <div
@@ -25,13 +25,30 @@ const MealMenuCard: React.FC<MenuItemCardProps & { mealType?: string }> = ({
       data-meal-type={mealType}
     >
       <div className="menu-item-content">
+        {/* {console.log("MealMenuCard props:", { id, Dish_name, ratings })} */}
         <div className="menu-item-name-container">
-          <h3 className="menu-item-name">{Dish_name[0].toUpperCase() + Dish_name.slice(1).toLowerCase()}</h3>
+          <div className="menu-item-name-wrapper">
+            <h3 className="menu-item-name">
+              {Dish_name[0].toUpperCase() + Dish_name.slice(1).toLowerCase()}
+            </h3>
+          </div>
           <p className={`menu-item-type ${typeColorClass}`}>{type}</p>
+
+          <div className="menu-item-bottom">
+            {/* <p className="menu-item-rating">
+              {relevantRatings.length === 0
+                ? "No ratings yet"
+                : `${averageRating} ⭐ (${relevantRatings.length} rating${
+                    relevantRatings.length > 1 ? "s" : ""
+                  })`}
+            </p> */}
+          </div>
         </div>
-        {isFeast && <div className="menu-item-feast-badge">Feast Special</div>}
+
+        {isFeast && (
+          <div className="menu-item-feast-badge">Feast Special</div>
+        )}
       </div>
-      
     </div>
   );
 };
